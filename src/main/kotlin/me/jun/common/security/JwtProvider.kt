7f867faf5ef10e653c0e154a5de7bc68161fdcc2
kjh9267 <intellijunho@kakao.com>
2024-09-03 12:33:14 +0900

    feat: Create jwtProvider -test: Create jwtProvider test

---
+++ src/main/kotlin/me/jun/common/security/JwtProvider.kt
@@ -0,0 +1,40 @@
+package me.jun.common.security
+
+import io.jsonwebtoken.JwtParser
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm.HS512
+import me.jun.common.security.exception.InvalidTokenException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+@Suppress("Deprecation")
+class JwtProvider(
+    @Value("\${jwt-key}") val jwtKey: String
+) {
+
+    fun createToken(email: String): String {
+        return Jwts.builder()
+            .setSubject(email)
+            .signWith(HS512, jwtKey)
+            .compact()
+    }
+
+    fun extractSubject(token: String): String {
+        try {
+            return createParser()
+                .parseClaimsJws(token)
+                .body
+                .subject
+        }
+        catch (e: Exception) {
+            throw InvalidTokenException.of(e.message)
+        }
+    }
+
+    private fun createParser(): JwtParser {
+        return Jwts.parser()
+            .setSigningKey(jwtKey)
+            .build()
+    }
+}
---
+++ src/test/kotlin/me/jun/support/TokenFixture.kt
@@ -0,0 +1,7 @@
+package me.jun.support
+
+const val JWT_KEY = "bUM2q6grP497hK4NKzCFbM9X8EBFbLgXXQXqmdShvNlkHNbQkk460rELLYxVA4cU+0rg9AIIl4ReodO5LKm2j+vlT+z844EznNTQzSEigdnGeE8E+KQNk4vmsBEqg8s4VJLSyv+EyadfrZMtmArlScxQaK69cmwppQ4Fv3hkB+U="
+
+const val TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhc2RmQGFzZGYuY29tIn0.1QKU8hkI1agism8PGbrmlQo6YC0gAppplw53V0OUr5226idTKz6ErWZDxmQ63FICwCNET3edYvKU678aq80ntg"
+
+const val EMAIL = "asdf@asdf.com"
