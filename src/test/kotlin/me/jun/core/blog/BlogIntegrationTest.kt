05e1d9dd30ac2326fee8726b5186c4181527c090
kjh9267 <intellijunho@kakao.com>
2024-09-06 20:38:23 +0900

    test: Create updateArticleTest in blog test

--- src/test/kotlin/me/jun/core/blog/BlogIntegrationTest.kt
+++ src/test/kotlin/me/jun/core/blog/BlogIntegrationTest.kt
@@ -5,6 +5,7 @@ import com.google.gson.JsonParser
 import io.restassured.RestAssured.given
 import me.jun.support.IntegrationTest
 import me.jun.support.createArticleRequest
+import me.jun.support.updateArticleRequest
 import org.hamcrest.Matchers.hasKey
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpHeaders.AUTHORIZATION
@@ -19,6 +20,7 @@ class BlogIntegrationTest: IntegrationTest() {
         login()
         createArticle()
         retrieveArticle(1L)
+        updateArticle()
     }
 
     private fun createArticle() {
@@ -56,6 +58,33 @@ class BlogIntegrationTest: IntegrationTest() {
 
             .`when`()
             .get("/api/blog/articles/$id")
+
+            .then()
+            .statusCode(OK.value())
+            .assertThat().body("$") { hasKey("id") }
+            .assertThat().body("$") { hasKey("title") }
+            .assertThat().body("$") { hasKey("content") }
+            .assertThat().body("$") { hasKey("writerId") }
+            .assertThat().body("$") { hasKey("createdAt") }
+            .assertThat().body("$") { hasKey("updatedAt") }
+            .extract()
+            .asString()
+
+        val element: JsonElement = JsonParser.parseString(response)
+        println(gson.toJson(element))
+    }
+
+    private fun updateArticle() {
+        val response: String = given()
+            .log().all()
+            .port(port!!)
+            .accept(APPLICATION_JSON_VALUE)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, token)
+            .body(updateArticleRequest())
+
+            .`when`()
+            .put("/api/blog/articles")
 
             .then()
             .statusCode(OK.value())
