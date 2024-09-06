30a59d36dad5548afea4debbc76a5bbdcb165523
kjh9267 <intellijunho@kakao.com>
2024-09-06 20:27:22 +0900

    test: Create retrieveArticleTest in blog test

--- src/test/kotlin/me/jun/core/blog/BlogIntegrationTest.kt
+++ src/test/kotlin/me/jun/core/blog/BlogIntegrationTest.kt
@@ -4,10 +4,11 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonParser
 import io.restassured.RestAssured.given
 import me.jun.support.IntegrationTest
-import me.jun.support.TOKEN
 import me.jun.support.createArticleRequest
+import org.hamcrest.Matchers.hasKey
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
 class BlogIntegrationTest: IntegrationTest() {
@@ -17,6 +18,7 @@ class BlogIntegrationTest: IntegrationTest() {
         register()
         login()
         createArticle()
+        retrieveArticle(1L)
     }
 
     private fun createArticle() {
@@ -32,13 +34,37 @@ class BlogIntegrationTest: IntegrationTest() {
             .post("/api/blog/articles")
 
             .then()
-//            .statusCode(OK.value())
-//            .assertThat().body("$") { hasKey("id") }
-//            .assertThat().body("$") { hasKey("title") }
-//            .assertThat().body("$") { hasKey("content") }
-//            .assertThat().body("$") { hasKey("writerId") }
-//            .assertThat().body("$") { hasKey("createdAt") }
-//            .assertThat().body("$") { hasKey("updatedAt") }
+            .statusCode(OK.value())
+            .assertThat().body("$") { hasKey("id") }
+            .assertThat().body("$") { hasKey("title") }
+            .assertThat().body("$") { hasKey("content") }
+            .assertThat().body("$") { hasKey("writerId") }
+            .assertThat().body("$") { hasKey("createdAt") }
+            .assertThat().body("$") { hasKey("updatedAt") }
+            .extract()
+            .asString()
+
+        val element: JsonElement = JsonParser.parseString(response)
+        println(gson.toJson(element))
+    }
+
+    private fun retrieveArticle(id: Long) {
+        val response: String = given()
+            .log().all()
+            .port(port!!)
+            .accept(APPLICATION_JSON_VALUE)
+
+            .`when`()
+            .get("/api/blog/articles/$id")
+
+            .then()
+            .statusCode(OK.value())
+            .assertThat().body("$") { hasKey("id") }
+            .assertThat().body("$") { hasKey("title") }
+            .assertThat().body("$") { hasKey("content") }
+            .assertThat().body("$") { hasKey("writerId") }
+            .assertThat().body("$") { hasKey("createdAt") }
+            .assertThat().body("$") { hasKey("updatedAt") }
             .extract()
             .asString()
 
