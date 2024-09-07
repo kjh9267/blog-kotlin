3575768356e87e19453829d37f1b4b3eae995b64
kjh9267 <intellijunho@kakao.com>
2024-09-07 21:23:22 +0900

    feat: Create postCountRepository -test: Create postCountRepository test

---
+++ src/test/kotlin/me/jun/core/guestbook/domain/repository/PostCountRepositoryTest.kt
@@ -0,0 +1,42 @@
+package me.jun.core.guestbook.domain.repository
+
+import me.jun.core.guestbook.domain.PostCount
+import me.jun.support.*
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class PostCountRepositoryTest {
+
+    @Autowired
+    private lateinit var postCountRepository: PostCountRepository
+
+    @Test
+    fun findByPostCountIdTest() {
+        val expected: PostCount = postCount()
+
+        postCountRepository.save(
+            PostCount(
+                postCountId = POST_COUNT_ID,
+                value = POST_COUNT_VALUE,
+                postId = POST_ID,
+                version = VERSION
+            )
+        )
+
+        assertThat(postCountRepository.findByPostCountId(POST_COUNT_ID))
+            .isEqualTo(expected)
+    }
+
+    @Test
+    fun noPostCount_findByPostCountIdFailTest() {
+        assertThat(postCountRepository.findByPostCountId(POST_COUNT_ID))
+            .isNull()
+    }
+}
